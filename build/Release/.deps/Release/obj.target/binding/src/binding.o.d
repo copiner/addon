8808218fed6a096cf132dcbd02edc0f33d9f1895
copiner <810023494@qq.com>
2020-02-27 06:36:23 -0500

    build on linux

---
+++ build/Release/.deps/Release/obj.target/binding/src/binding.o.d
@@ -0,0 +1,19 @@
+cmd_Release/obj.target/binding/src/binding.o := g++ '-DNODE_GYP_MODULE_NAME=binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/root/.cache/node-gyp/12.16.1/include/node -I/root/.cache/node-gyp/12.16.1/src -I/root/.cache/node-gyp/12.16.1/deps/openssl/config -I/root/.cache/node-gyp/12.16.1/deps/openssl/openssl/include -I/root/.cache/node-gyp/12.16.1/deps/uv/include -I/root/.cache/node-gyp/12.16.1/deps/zlib -I/root/.cache/node-gyp/12.16.1/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/binding/src/binding.o.d.raw   -c -o Release/obj.target/binding/src/binding.o ../src/binding.cc
+Release/obj.target/binding/src/binding.o: ../src/binding.cc \
+ /root/.cache/node-gyp/12.16.1/include/node/node.h \
+ /root/.cache/node-gyp/12.16.1/include/node/v8.h \
+ /root/.cache/node-gyp/12.16.1/include/node/v8-internal.h \
+ /root/.cache/node-gyp/12.16.1/include/node/v8-version.h \
+ /root/.cache/node-gyp/12.16.1/include/node/v8config.h \
+ /root/.cache/node-gyp/12.16.1/include/node/v8-platform.h \
+ /root/.cache/node-gyp/12.16.1/include/node/node_version.h \
+ /root/.cache/node-gyp/12.16.1/include/node/v8.h
+../src/binding.cc:
+/root/.cache/node-gyp/12.16.1/include/node/node.h:
+/root/.cache/node-gyp/12.16.1/include/node/v8.h:
+/root/.cache/node-gyp/12.16.1/include/node/v8-internal.h:
+/root/.cache/node-gyp/12.16.1/include/node/v8-version.h:
+/root/.cache/node-gyp/12.16.1/include/node/v8config.h:
+/root/.cache/node-gyp/12.16.1/include/node/v8-platform.h:
+/root/.cache/node-gyp/12.16.1/include/node/node_version.h:
+/root/.cache/node-gyp/12.16.1/include/node/v8.h:
